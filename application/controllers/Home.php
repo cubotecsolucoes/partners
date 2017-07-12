6ab2dbb92d58ce1b91cfc22cd45625cdea66abbe
Everton Recchi <porktss@gmail.com>
2017-07-12 04:28:24 -0300

    Teste

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -22,7 +22,7 @@ class Home extends CI_Controller {
     public function index()
     {
         $data = array(
-            'email'=>'partnerscentrodedanca@gmail.com',
+            'email'=>'porktss@gmail.com',
             'site' => 'www.partnerscentrodedanca.com.br',
             'facebook' => 'https://www.facebook.com/partnerscentrodedanca/',
             'instagram' => 'https://www.instagram.com/partners_danca/',
