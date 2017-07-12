ca844999c6a0b9416fd2809d551375a472877090
Everton Recchi <porktss@gmail.com>
2017-07-12 04:39:37 -0300

    Update style

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -34,44 +34,35 @@ class Home extends CI_Controller {
 
     public function contato()
     {
-        $this->load->library('email');
-
-        $this->email->from($_POST['email'], $_POST['nome']);
-        $this->email->to('porktss@gmail.com');
-
-        $this->email->subject('Email Test');
-        $this->email->message('Testing the email class.');
-
-        $this->email->send();
-        // $nome=$_POST['nome'];
-        // $email=$_POST['email'];
-        // $Titulo=$_POST['assunto'];
-        // $texto=$_POST['texto'];
-        // $Destinatario="porktss@gmail.com";
-        // $mensagem1="
-        // <h2>Mensagem recebida do site do Evento!</h2>
+        $nome=$_POST['nome'];
+        $email=$_POST['email'];
+        $Titulo=$_POST['assunto'];
+        $texto=$_POST['texto'];
+        $Destinatario="porktss@gmail.com";
+        $mensagem1="
+        <h2>Mensagem recebida do site do Evento!</h2>
         
-        // <h3>Nome: <b>$nome</b></h3>
-        // <h3>Email: $email</h3>
+        <h3>Nome: <b>$nome</b></h3>
+        <h3>Email: $email</h3>
         
-        // Mensagem: <p>$texto</p>
+        Mensagem: <p>$texto</p>
 
-        // <a href=\"mailto:$email\"><button style=\"background-color: green; width: 70px; height: 40px; border: 1px solid grey;\">Responder</button></a>
-        // ";
+        <a href=\"mailto:$email\"><button style=\"background-color: grey; width: 90px; height: 40px; border: 1px solid grey;\">Responder</button></a>
+        ";
 
-        // $headers = "MIME-Version: 1.0" . "\r\n";
-        // $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
-        // $headers .= "From: $Destinatario" . "\r\n" .
-        // "Reply-To: $email" . "\r\n" .
-        // "X-Mailer: PHP/" . phpversion();
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
+        $headers .= "From: $Destinatario" . "\r\n" .
+        "Reply-To: $email" . "\r\n" .
+        "X-Mailer: PHP/" . phpversion();
 
-        // // ENVIA O EMAIL
-        // $call = mail("$Destinatario","$Titulo", "$mensagem1",$headers);
-        // if (!$call) {
-        //     $data=['erro'=>'Não foi possivel enviar a mensagem'];
-        // } else {
-        //     $data=['sucesso'=>'Mensagem enviada com sucesso!'];
-        // }
+        // ENVIA O EMAIL
+        $call = mail("$Destinatario","$Titulo", "$mensagem1",$headers);
+        if (!$call) {
+            $data=['erro'=>'Não foi possivel enviar a mensagem'];
+        } else {
+            $data=['sucesso'=>'Mensagem enviada com sucesso!'];
+        }
 
         echo(json_encode($data));
     }
