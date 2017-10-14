bd3c5edd2b04f0dcad73ea05f876f0be7d674111
Everton Recchi <porktss@gmail.com>
2017-10-14 16:17:28 -0300

    ah jesus quanta atualização

--- application/views/reservas/painel.php
+++ application/views/reservas/painel.php
@@ -268,8 +268,9 @@
 
 //        RETIRAR DEPOIS
 
-        var concatenar = '<h1>A marcação do assentos e impressão dos ingressos só será realizada no dia 28/10 a partir das 8:00</h1>';
+        var concatenar = '<h1 style="color: red">A marcação do assentos e impressão dos ingressos só será realizada no dia 28/10 a partir das 8:00</h1>';
         lugares.html(concatenar);
+        lug.show('slow');
 //        FIM DE RETIRAR DEPOIS
 
 //    aliVal = $(this).attr('data-valor');
